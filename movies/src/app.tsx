93262a0bd42633b7bace842d97b17a09237c2702
rogervalentim <valentim.roger33@gmail.com>
2024-03-24 19:42:00 -0300

    chore: react query installation

--- movies/src/app.tsx
+++ movies/src/app.tsx
@@ -1,13 +1,3 @@
-// import { Card, CardContent } from "./components/ui/card";
-
 export function App() {
-  return (
-    <div>
-      {/* <Card className="bg-black">
-        <CardContent>
-          <p>movies</p>
-        </CardContent>
-      </Card> */}
-    </div>
-  );
+  return <div />;
 }
--- movies/src/main.tsx
+++ movies/src/main.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "@/index.css";
 import { App } from "./app";
+import "@/index.css";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
