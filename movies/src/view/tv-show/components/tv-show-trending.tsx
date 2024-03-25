1a79072b990c9d08829befd141b4bf175f91aedb
rogervalentim <valentim.roger33@gmail.com>
2024-03-24 21:41:05 -0300

    feat: add tv-show-trending

---
+++ movies/src/view/tv-show/components/tv-show-trending.tsx
@@ -0,0 +1,52 @@
+import { useQuery } from "@tanstack/react-query";
+import { TmdbProps } from "@/types/types";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem
+} from "@/components/ui/carousel";
+import { Card } from "@/components/card";
+import { Link } from "react-router-dom";
+import { SkeletonCategory } from "@/components/skeleton-category";
+
+const apiKey = "fb8991fa7ac1a75e1cd0dd9805822f4c";
+
+export function TvShowsTrending() {
+  const { data: tvShowsTrending, isLoading } = useQuery<TmdbProps[]>({
+    queryKey: ["get-trending-tv"],
+    queryFn: async () => {
+      const response = await fetch(
+        `https://api.themoviedb.org/3/trending/tv/day?api_key=${apiKey}&language=pt-BR`
+      );
+      const data = await response.json();
+
+      return data.results;
+    }
+  });
+
+  if (isLoading) {
+    return <SkeletonCategory />;
+  }
+
+  return (
+    <>
+      <Carousel>
+        <CarouselContent className="-ml-2 py-2 md:-ml-4">
+          {tvShowsTrending &&
+            tvShowsTrending.map((tv: TmdbProps) =>
+              <Link to={`/tv/${tv.id}`} key={tv.id}>
+                <CarouselItem className="relative w-40 basis-1/3 md:basis-1/4 md:w-52 lg:w-64">
+                  <Card
+                    id={tv.id}
+                    poster_path={tv.poster_path}
+                    title={tv.name}
+                    vote_average={tv.vote_average}
+                  />
+                </CarouselItem>
+              </Link>
+            )}
+        </CarouselContent>
+      </Carousel>
+    </>
+  );
+}
